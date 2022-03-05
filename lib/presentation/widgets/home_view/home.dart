3d4c260ee3ec12ca93214adc1e2aca1ad0fd7b49
Suman Adhikari <expertxkid@gmail.com>
2022-03-05 22:11:52 +0545

    refactor: add app bar in home page

--- lib/presentation/widgets/home_view/home.dart
+++ lib/presentation/widgets/home_view/home.dart
@@ -8,7 +8,38 @@ class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: AppColors.primaryColor,
+      drawer: const Drawer(),
+      appBar: AppBar(
+        leading: Builder(builder: (buildContext) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(buildContext).colorScheme.secondary,
+                borderRadius: BorderRadius.circular(50),
+              ),
+              width: 20,
+              height: 20,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.menu,
+                  color: Colors.white,
+                ),
+                onPressed: () {
+                  if (Scaffold.of(buildContext).hasDrawer &&
+                      !Scaffold.of(buildContext).isDrawerOpen) {
+                    Scaffold.of(buildContext).openDrawer();
+                  }
+                },
+              ),
+            ),
+          );
+        }),
+        title: const Image(
+            height: 32,
+            image: AssetImage('assets/logos/icon_with_brand_name.png')),
+      ),
+      backgroundColor: AppColors.scaffoldBackgroundColor,
       body: Container(),
     );
   }
