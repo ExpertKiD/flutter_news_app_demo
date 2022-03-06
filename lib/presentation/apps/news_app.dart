ce1074387f1d066d365e9feae0dfd1af99aa0dce
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 17:05:29 +0545

    feat: add all specific news providers for news app to MultiBlocProvider instance

--- lib/presentation/apps/news_app.dart
+++ lib/presentation/apps/news_app.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/business_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/entertainment_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/general_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/health_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/main_headline_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/science_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/technology_news_bloc.dart';
+import 'package:news_app/data/providers/news_api_org.dart';
+import 'package:news_app/data/repositories/news.dart';
 
+import '../../business_logic/blocs/news/sports_news_bloc.dart';
+import '../../main.dart';
 import '../routers/app_router.dart';
 import '../themes/theme.dart';
 
@@ -8,12 +20,74 @@ class NewsApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: lightTheme,
-      darkTheme: darkTheme,
-      themeMode: ThemeMode.light,
-      title: 'Naya Khabar',
-      onGenerateRoute: AppRouter.onGenerateRoute,
+    return MultiBlocProvider(
+      providers: _providers(),
+      child: MaterialApp(
+        theme: lightTheme,
+        darkTheme: darkTheme,
+        themeMode: ThemeMode.light,
+        title: 'Naya Khabar',
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
     );
   }
+
+  List<BlocProvider> _providers() => [
+        BlocProvider(
+          create: (context) => MainHeadlineNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => BusinessNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => EntertainmentNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => GeneralNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => HealthNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ScienceNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => SportsNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => TechnologyNewsBloc(
+            newsRepository: NewsRepository(
+              NewsAPI(dio: dio),
+            ),
+          ),
+        ),
+      ];
 }
