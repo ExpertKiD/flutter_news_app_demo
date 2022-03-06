2cb839ae66649869679d4ada53ea6b928e04a8e9
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 20:56:30 +0545

    refactor: update TopHeadlinesView with FeaturedNewsLayoutWidget

--- lib/presentation/widgets/home_view/top_headlines_view.dart
+++ lib/presentation/widgets/home_view/top_headlines_view.dart
@@ -1,10 +1,37 @@
 part of 'home.dart';
 
-class TopHeadlinesView extends StatelessWidget {
+class TopHeadlinesView extends StatefulWidget {
   const TopHeadlinesView({Key? key}) : super(key: key);
 
   @override
+  State<TopHeadlinesView> createState() => _TopHeadlinesViewState();
+}
+
+class _TopHeadlinesViewState extends State<TopHeadlinesView> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<MainHeadlineNewsBloc>().add(NewsEvent.newsLoadStarted());
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return BlocBuilder<MainHeadlineNewsBloc, NewsState>(
+      builder: (context, state) {
+        if (state is NewsInitial || state is NewsLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is NewsLoadSuccess) {
+          return FeaturedNewsLayoutWidget(
+            articles: state.news.articles,
+            title: 'Main News',
+            maxVisibleArticles: 6,
+          );
+        } else {
+          return const SizedBox(
+            height: 0,
+          );
+        }
+      },
+    );
   }
 }
