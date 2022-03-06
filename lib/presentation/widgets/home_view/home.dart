e111c9d6a882e1ef066d2ee700c62cee53a4079f
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 20:57:40 +0545

    refactor: update HomeView to show ListView with TopHeadlinesView

--- lib/presentation/widgets/home_view/home.dart
+++ lib/presentation/widgets/home_view/home.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/main_headline_news_bloc.dart';
+import 'package:news_app/business_logic/events/news_event/news_event.dart';
+import 'package:news_app/business_logic/states/news_state/news_state.dart';
+import 'package:news_app/presentation/widgets/news_layouts/featured_news_layout.dart';
 
+import '../../../business_logic/states/news_state/news_state.dart';
 import '../../../constants/colors.dart';
+
+part 'top_headlines_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({Key? key}) : super(key: key);
@@ -11,7 +20,12 @@ class HomeView extends StatelessWidget {
       drawer: const Drawer(),
       appBar: _appBar(context),
       backgroundColor: AppColors.scaffoldBackgroundColor,
-      body: Container(),
+      body: ListView(
+          scrollDirection: Axis.vertical,
+          physics: const AlwaysScrollableScrollPhysics(),
+          children: const [
+            TopHeadlinesView(),
+          ]),
     );
   }
 
