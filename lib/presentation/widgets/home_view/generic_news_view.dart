047e00e4fd79dcb110642f8604c42cd0dbe07e3e
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 21:32:40 +0545

    refactor: rename health_news_view to generic_news_view.dart

---
+++ lib/presentation/widgets/home_view/generic_news_view.dart
@@ -0,0 +1,57 @@
+part of 'home.dart';
+
+class GenericNewsView<BlocA extends Bloc<NewsEvent, NewsState>>
+    extends StatefulWidget {
+  final int maxVisibleArticles;
+  final String categoryTitle;
+
+  const GenericNewsView({
+    Key? key,
+    required this.maxVisibleArticles,
+    required this.categoryTitle,
+  }) : super(key: key);
+
+  @override
+  State<GenericNewsView> createState() => _GenericNewsViewState<BlocA>(
+        maxVisibleArticles: maxVisibleArticles,
+        categoryTitle: categoryTitle,
+      );
+}
+
+class _GenericNewsViewState<BlocA extends Bloc<NewsEvent, NewsState>>
+    extends State<GenericNewsView> {
+  final int maxVisibleArticles;
+  final String categoryTitle;
+
+  _GenericNewsViewState({
+    required this.maxVisibleArticles,
+    required this.categoryTitle,
+  });
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<BlocA>().add(NewsEvent.newsLoadStarted());
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<BlocA, NewsState>(
+      builder: (context, state) {
+        if (state is NewsInitial || state is NewsLoading) {
+          return const Center(child: CircularProgressIndicator());
+        } else if (state is NewsLoadSuccess) {
+          return FeaturedNewsLayoutWidget(
+            articles: state.news.articles,
+            title: categoryTitle,
+            maxVisibleArticles: maxVisibleArticles,
+          );
+        } else {
+          return const SizedBox(
+            height: 0,
+          );
+        }
+      },
+    );
+  }
+}
