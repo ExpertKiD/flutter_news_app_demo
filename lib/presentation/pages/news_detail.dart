a27b52837b9a6959d2e63aa5f58ba202f633dd3c
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 22:45:03 +0545

    feat: add article as argument in NewsDetailPage

--- lib/presentation/pages/news_detail.dart
+++ lib/presentation/pages/news_detail.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:news_app/presentation/widgets/news_detail_view/news_detail.dart';
 
+import '../../data/models/news_provider/article.dart';
+
 class NewsDetailPage extends StatelessWidget {
-  const NewsDetailPage({Key? key}) : super(key: key);
+  final Article article;
+  const NewsDetailPage({Key? key, required this.article}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const NewsDetailView();
+    return NewsDetailView(article: article);
   }
 }
