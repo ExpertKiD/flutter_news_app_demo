3c7ad5e20f0f9543d50942bf55bcaa24c61d9049
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 14:55:39 +0545

    feat: add dio http client global instance

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_app/presentation/apps/news_app.dart';
+
+final dio = Dio();
 
 Future main() async {
   await dotenv.load(fileName: '.env');
