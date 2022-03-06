d2458ec0dbb4947dcce762ffc9cbb304dbf817a8
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 21:56:51 +0545

    feat: add GenericNewsView on home view for each category-wise news

--- lib/presentation/widgets/home_view/home.dart
+++ lib/presentation/widgets/home_view/home.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/business_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/entertainment_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/general_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/health_news_bloc.dart';
 import 'package:news_app/business_logic/blocs/news/main_headline_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/science_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/sports_news_bloc.dart';
+import 'package:news_app/business_logic/blocs/news/technology_news_bloc.dart';
 import 'package:news_app/business_logic/events/news_event/news_event.dart';
 import 'package:news_app/business_logic/states/news_state/news_state.dart';
+import 'package:news_app/data/enums/news_category.dart';
 import 'package:news_app/presentation/widgets/news_layouts/featured_news_layout.dart';
+import 'package:news_app/utils/extensions/news_category.dart';
+import 'package:recase/recase.dart';
 
 import '../../../business_logic/states/news_state/news_state.dart';
 import '../../../constants/colors.dart';
 
+part 'generic_news_view.dart';
 part 'top_headlines_view.dart';
 
 class HomeView extends StatelessWidget {
@@ -21,11 +31,47 @@ class HomeView extends StatelessWidget {
       appBar: _appBar(context),
       backgroundColor: AppColors.scaffoldBackgroundColor,
       body: ListView(
-          scrollDirection: Axis.vertical,
-          physics: const AlwaysScrollableScrollPhysics(),
-          children: const [
-            TopHeadlinesView(),
-          ]),
+        children: [
+          GenericNewsView<MainHeadlineNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: 'Main news',
+          ),
+          GenericNewsView<BusinessNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.business.value,
+          ),
+          GenericNewsView<EntertainmentNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.entertainment.value,
+          ),
+          GenericNewsView<GeneralNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.general.value,
+          ),
+          GenericNewsView<HealthNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.health.value,
+          ),
+          GenericNewsView<HealthNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.health.value,
+          ),
+          GenericNewsView<ScienceNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.science.value,
+          ),
+          GenericNewsView<SportsNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.sports.value,
+          ),
+          GenericNewsView<TechnologyNewsBloc>(
+            maxVisibleArticles: 6,
+            categoryTitle: NewsCategory.sports.value,
+          ),
+        ],
+        scrollDirection: Axis.vertical,
+        physics: const AlwaysScrollableScrollPhysics(),
+      ),
     );
   }
 
