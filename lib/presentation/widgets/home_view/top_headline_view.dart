c59f2662ecddf669e9722cfa7f446de2d72f79a4
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 12:52:56 +0545

    feat: add dio package

---
+++ lib/presentation/widgets/home_view/top_headline_view.dart
@@ -0,0 +1,10 @@
+part of 'home.dart';
+
+class TopHeadlinesView extends StatelessWidget {
+  const TopHeadlinesView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
