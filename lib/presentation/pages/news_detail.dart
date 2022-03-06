dc14feafdce4b3c023856dee3776812ecae5afae
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 22:11:18 +0545

    feat: add news detail page and view

---
+++ lib/presentation/pages/news_detail.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:news_app/presentation/widgets/news_detail_view/news_detail.dart';
+
+class NewsDetailPage extends StatelessWidget {
+  const NewsDetailPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const NewsDetailView();
+  }
+}
