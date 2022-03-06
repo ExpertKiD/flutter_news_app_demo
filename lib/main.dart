8b98134c62bcdc23f6c891a57a6c9a901bd2db82
Suman Adhikari <expertxkid@gmail.com>
2022-03-06 17:05:53 +0545

    feat: add custom BlocObserver for debug

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,20 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_app/presentation/apps/news_app.dart';
+
+import 'business_logic/observers/observer.dart';
 
 final dio = Dio();
 
 Future main() async {
-  await dotenv.load(fileName: '.env');
+  BlocOverrides.runZoned(
+    () async {
+      await dotenv.load(fileName: '.env');
 
-  runApp(const NewsApp());
+      runApp(const NewsApp());
+    },
+    blocObserver: MyBlocObserver(),
+  );
 }
