c3249d0c75409747383fcb8714b109f480f9ae20
Suman Adhikari <expertxkid@gmail.com>
2022-03-07 12:37:52 +0545

    feat: add hive cache store to store all http requests except post requests and http 401 and 403 requests while using the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
 import 'package:dio/dio.dart';
+import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
+import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:news_app/presentation/apps/news_app.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'business_logic/observers/observer.dart';
 
@@ -12,6 +15,20 @@ Future main() async {
   BlocOverrides.runZoned(
     () async {
       await dotenv.load(fileName: '.env');
+
+      final tempDirectory = await getTemporaryDirectory().then((dir) {
+        dio.interceptors.add(DioCacheInterceptor(
+          options: CacheOptions(
+            store: HiveCacheStore(dir.path),
+            hitCacheOnErrorExcept: [401, 403],
+            policy: CachePolicy.forceCache,
+            allowPostMethod: false,
+            priority: CachePriority.high,
+            keyBuilder: CacheOptions.defaultCacheKeyBuilder,
+            maxStale: const Duration(hours: 1),
+          ),
+        ));
+      });
 
       runApp(const NewsApp());
     },
