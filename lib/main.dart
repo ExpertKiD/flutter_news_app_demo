1a4dfa87f237a61b093e8c5b0da7902f62fd7c67
Suman Adhikari <expertxkid@gmail.com>
2022-03-07 13:04:54 +0545

    fix: fix issue with path provider failing to provide path due to zone usage

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:dio/dio.dart';
 import 'package:dio_cache_interceptor/dio_cache_interceptor.dart';
 import 'package:dio_cache_interceptor_hive_store/dio_cache_interceptor_hive_store.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:news_app/presentation/apps/news_app.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -12,14 +12,20 @@ import 'business_logic/observers/observer.dart';
 final dio = Dio();
 
 Future main() async {
-  BlocOverrides.runZoned(
+  WidgetsFlutterBinding.ensureInitialized();
+
+  final documentsDirectory = await getApplicationDocumentsDirectory();
+  final hydratedBlocStorage =
+      await HydratedStorage.build(storageDirectory: documentsDirectory);
+
+  HydratedBlocOverrides.runZoned(
     () async {
       await dotenv.load(fileName: '.env');
 
-      await getApplicationDocumentsDirectory().then((dir) {
-        dio.interceptors.add(DioCacheInterceptor(
+      dio.interceptors.add(
+        DioCacheInterceptor(
           options: CacheOptions(
-            store: HiveCacheStore(dir.path),
+            store: HiveCacheStore(documentsDirectory.path),
             hitCacheOnErrorExcept: [401, 403],
             policy: CachePolicy.forceCache,
             allowPostMethod: false,
@@ -27,11 +33,12 @@ Future main() async {
             keyBuilder: CacheOptions.defaultCacheKeyBuilder,
             maxStale: const Duration(hours: 1),
           ),
-        ));
-      });
+        ),
+      );
 
       runApp(const NewsApp());
     },
+    storage: hydratedBlocStorage,
     blocObserver: MyBlocObserver(),
   );
 }
